d5e1fafec8b78bdb64eb620b1787081dd5e4dfa6
MonsterCrafter707 <kaupertalex@gmail.com>
2025-02-06 10:29:15 +0100

    Revert "Whoop whoop"
    This reverts commit 89e31f1cb90a92935e1a8eec578819d75ae21948.

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -1,31 +1,24 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
  
-public class DoorOpen : MonoBehaviour, IPointerClickHandler,IPointerEnterHandler, IPointerExitHandler 
+public class DoorOpen : MonoBehaviour, IPointerClickHandler,IPointerEnterHandler, IPointerExitHandler
 {
     public Sprite normalImage;
     public Sprite pressedImage;
-    public bool doorOpen = false;
+    bool doorOpen = false;
     private Image buttonImage;
-    //public Scene scene;
-    public string sceneName;
  
     void Start()
     {
         buttonImage = GetComponent<Image>();
-        //scene = GetComponent<Scene>();
     }
  
     public void OnPointerClick(PointerEventData eventData)
     {
         buttonImage.sprite = pressedImage;
         doorOpen = true;
-        SceneManager.LoadScene(sceneName);
-        
     }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
         buttonImage.sprite = pressedImage;

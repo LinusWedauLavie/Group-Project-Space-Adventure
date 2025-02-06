affbc0146edef5dadf5f88ec90578bb748001884
MonsterCrafter707 <kaupertalex@gmail.com>
2025-02-06 10:34:32 +0100

    Whoop whoop

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -1,29 +1,36 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
  
-public class DoorOpen : MonoBehaviour, IPointerClickHandler,IPointerEnterHandler, IPointerExitHandler
+public class DoorOpen : MonoBehaviour, IPointerClickHandler,IPointerEnterHandler, IPointerExitHandler 
 {
     public Sprite normalImage;
     public Sprite pressedImage;
-    bool doorOpen = false;
+    public bool doorOpen = false;
     private Image buttonImage;
+    //public Scene scene;
+    public string sceneName;
  
     void Start()
     {
         buttonImage = GetComponent<Image>();
+        //scene = GetComponent<Scene>();
     }
  
     public void OnPointerClick(PointerEventData eventData)
     {
         buttonImage.sprite = pressedImage;
         doorOpen = true;
+        SceneManager.LoadScene(sceneName);
+        
     }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         buttonImage.sprite = pressedImage;
     }
-    public void OnPointerExit(PointerEventData eventData)
+        public void OnPointerExit(PointerEventData eventData)
     {
         if (doorOpen == false)
         {

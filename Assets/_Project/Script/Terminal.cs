64d2bdc6add91a5fe3597246389bf5ca7db7e783
KevinTheLamb <Kevin.Waskow@bvb.lavie-reha.de>
2025-02-23 00:11:48 +0100

    HangarStuffPolishAusLangeweile

--- Assets/_Project/Script/Terminal.cs
+++ Assets/_Project/Script/Terminal.cs
@@ -5,28 +5,44 @@ using UnityEngine.UI;
 public class Terminal : MonoBehaviour
 {
         private Image buttonImage;
-        public GameObject forcefields;
-        public Camera cam;
 
-    public Transform startCamPos;
+    public SpriteRenderer forcefieldRendererUp, forcefieldRendererDown;
+
+    Color color;
+    Color normalColor;
+
+    public float alphaValue = 0.33f; //beim anderen 0.03 oder so
+
+    public Camera cam;
+
+    Transform startCamPos;
     public Transform targetTransform;
     public float transitionSpeed = 2.0f;
     public float maxZPosition = -5.0f;
+
+    public GameObject GoBackFromMapButton, GoBackFromMapButtonArrows, ActivateTerminalButton;
 
     void Start()
     {
         buttonImage = GetComponent<Image>();
         startCamPos = cam.transform;
+        normalColor = forcefieldRendererUp.color;
     }
  
     public void ClickedTerminal()
     {
-            StartCoroutine(OpenMap());
+        color = normalColor;
+        alphaValue = 0.33f;
+        ActivateTerminalButton.GetComponent<Button>().enabled = false;
+        StartCoroutine(OpenMap());
     }
     public void BackAwayFromPlanetMap()
     {
-        
 
+        color = normalColor;
+        alphaValue = 0.03f;
+        GoBackFromMapButtonArrows.SetActive(false);
+        GoBackFromMapButton.GetComponent<Button>().enabled = false;
         StartCoroutine(CloseMap());
     }
 
@@ -41,9 +57,27 @@ public class Terminal : MonoBehaviour
         cam.transform.position = newPosition;
         cam.transform.rotation = Quaternion.Slerp(transform.rotation, targetTransform.rotation, Time.deltaTime * transitionSpeed);
 
+        if (alphaValue > 0.03f)
+        {
+            alphaValue -= 0.005f;
+            color.a = alphaValue;
+            forcefieldRendererUp.color = color;
+            forcefieldRendererDown.color = color;
+        }
+        else
+        {
+            alphaValue = 0.03f;
+        }
+
         if (cam.transform.position.z < -5.8f)
         {
             StartCoroutine(OpenMap());
+        }
+        else
+        {
+            GoBackFromMapButton.SetActive(true);
+            GoBackFromMapButtonArrows.SetActive(true);
+            GoBackFromMapButton.GetComponent<Button>().enabled = true;
         }
 
     }
@@ -53,15 +87,33 @@ public class Terminal : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
 
-        Vector3 targetpos5less = new Vector3(0, 0, startCamPos.position.z + maxZPosition+4);
+        Vector3 targetpos5less = new Vector3(0, 0, startCamPos.position.z + maxZPosition + 4);
         Vector3 newPosition = Vector3.Lerp(cam.transform.position, targetpos5less, Time.deltaTime * transitionSpeed);
 
         cam.transform.position = newPosition;
         cam.transform.rotation = Quaternion.Slerp(transform.rotation, startCamPos.rotation, Time.deltaTime * transitionSpeed);
 
+
+        if (color.a < 0.33f)
+        {
+            alphaValue += 0.005f;
+            color.a = alphaValue;
+            forcefieldRendererUp.color = color;
+            forcefieldRendererDown.color = color;
+        }
+        else
+        {
+            alphaValue = 0.33f;
+        }
+
         if (cam.transform.position.z > -13.8f)
         {
             StartCoroutine(CloseMap());
+        }
+        else
+        {
+            GoBackFromMapButton.SetActive(false);
+            ActivateTerminalButton.GetComponent<Button>().enabled = true;
         }
 
     }

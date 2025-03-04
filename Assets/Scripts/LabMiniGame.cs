a67066a113bce078c2e7d7bcb2b92c103e9e03a2
MonsterCrafter707 <kaupertalex@gmail.com>
2025-03-04 10:15:09 +0100

    sd

---
+++ Assets/Scripts/LabMiniGame.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LabMiniGame : MonoBehaviour
+{
+    public Sprite empty;
+    public Sprite oneLayer;
+    public Sprite twoLayers;
+    public Sprite full;
+    int level = 0;
+    UnityEngine.UI.Image buttonImage;
+
+    void Start()
+    {
+        buttonImage = GetComponent<UnityEngine.UI.Image>();
+    }
+    void Update()
+    {
+        if(level == 0)
+        {   
+            buttonImage.sprite = empty; 
+        }
+        else if (level == 1)
+        {   
+            buttonImage.sprite = oneLayer; 
+        }
+        else if (level == 2)
+        {   
+            buttonImage.sprite = twoLayers; 
+        }
+        else if (level == 3)
+        {   
+            buttonImage.sprite = full; 
+        }
+    }
+
+    public void OnButtonPress()
+    {
+        level += 1;
+        if (level > 3)
+        {   
+            level = 0;
+        }
+    }
+}

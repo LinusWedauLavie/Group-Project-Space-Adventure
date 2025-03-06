2b68c11a31dbb46cad4fc6ad7d751edf2075ad70
Kevin Waskow <Kevin.Waskow@bvb.lavie-reha.de>
2025-03-06 15:40:36 +0100

    Maer hangar zeugs baaa

--- Assets/_Project/Script/RohstoffLager.cs
+++ Assets/_Project/Script/RohstoffLager.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class RohstoffLager : MonoBehaviour
 {
-[SerializeField] public Observable<float> Uran, Chemikalien, WeltraumSchrott, Legierungen, Mundspülung, Zitronensäure, TerraSchlangenGift; // Als items Mundspülung, Zitronensäure, TerraSchlangenGift
+    [SerializeField] public Observable<float> Uran, Chemikalien, WeltraumSchrott, MetallErz, Legierungen, Mundspülung, Zitronensäure, TerraSchlangenGift; // Als items Mundspülung, Zitronensäure, TerraSchlangenGift
+    static RohstoffLager instance;
 
+    public int miningDrones = 1;
+
+
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            Debug.Log("RohstoffLager created");
+        }
+    }
 
     void Start()
     {
@@ -12,7 +30,9 @@ public class RohstoffLager : MonoBehaviour
 
     public void StartValues()
     {
-        Uran.Value = 5 ; Chemikalien.Value = 10; WeltraumSchrott.Value = 15;
-        Legierungen.Value = 0; Mundspülung.Value = 0; Zitronensäure.Value = 0; TerraSchlangenGift.Value = 0;
+        Uran.Value = 5; Chemikalien.Value = 10; WeltraumSchrott.Value = 15;
+        MetallErz.Value = 0; Legierungen.Value = 0; Mundspülung.Value = 0; Zitronensäure.Value = 0; TerraSchlangenGift.Value = 0;
     }
+
+
 }

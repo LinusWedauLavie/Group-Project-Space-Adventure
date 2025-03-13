824365f3e8c5ec19a6db6f120e3a5a7687355ddf
KevinTheLamb <Kevin.Waskow@bvb.lavie-reha.de>
2025-03-13 10:56:38 +0100

    Mikrowelle läuft, fangt sie ein!!!

--- Assets/Scripts/Mikrowelle.cs
+++ Assets/Scripts/Mikrowelle.cs
@@ -1,17 +1,54 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Mikrowelle : MonoBehaviour
 {
-    public Item currentItem;
-    InventoryManager inventoryManager;
+    public Animator animator;
+    public RohstoffLager rohstoffLager;
+    public DialogueBox dialogueBox;
+
+    bool isCurrentlyCooking;
     void Start()
     {
-        inventoryManager = FindAnyObjectByType<InventoryManager>();
-
+        rohstoffLager = FindFirstObjectByType<RohstoffLager>();
+        dialogueBox = FindFirstObjectByType<DialogueBox>();
+    }
+    public void MikroWelleClicked()
+    {
+        if (isCurrentlyCooking)
+        {
+            return;
+        }
+        if (rohstoffLager == null)
+        {
+            dialogueBox.ShowText("Eine...Mikrowelle? Da kann ich bestimmt Erze aus dem Hangar zu Legierungen schmelzen! \nIch brÄuche 1 MetallErz und 1 Weltraumschrott dafÜr...");
+            return;
+        }
+        else if (rohstoffLager.MetallErz.Value >= 1 && rohstoffLager.WeltraumSchrott.Value >= 1)
+        {
+            StartCoroutine(PlayAnim());
+        }
+        else
+        {
+            dialogueBox.ShowText("Eine...Mikrowelle? Da kann ich bestimmt Erze aus dem Hangar zu Legierungen schmelzen! \nIch brÄuche 1 MetallErz und 1 Weltraumschrott dafÜr...");
+        }
     }
 
-    void Update()
+    IEnumerator PlayAnim()
     {
-        
+        isCurrentlyCooking = true;
+        animator.SetBool("IsRunning", true);
+        yield return new WaitForSeconds(2.4f);
+
+
+        while (rohstoffLager.MetallErz.Value >= 1 && rohstoffLager.WeltraumSchrott.Value >= 1)
+        {
+            rohstoffLager.MetallErz.Value--;
+            rohstoffLager.WeltraumSchrott.Value--;
+            rohstoffLager.Legierungen.Value++;
+        }
+        isCurrentlyCooking = false;
+        animator.SetBool("IsRunning", false);
     }
 }
